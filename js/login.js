0eb4f7f7e1c1d0cc53cc375c7c1f3687faefbeb2
barriosx <stevenbarrios03@gmail.com>
2017-04-17 20:57:33 -0400

    Useful js code

--- js/login.js
+++ js/login.js
@@ -1,74 +1,79 @@
 var url;
 var method;
 var data={};
-var responsedata={}; // test variable
-var htmlRedirect;
+/*
+  Using a combination of JQuery and a javascript plugin called animated which makes the page more dynamic
+*/
 $(document.body).ready(function() {
   $('.form').addClass('animated fadeIn');
   $('.hdr').addClass('animated fadeIn');
 });
-// Method that toggles both login and registration forms. This method relies on display style  of
+
+// JQuery method that toggles the display between both login and registration forms. This method relies on display style  of
 // the reset form to be none
 $('.prompt a').on('click',function(){
   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+
+// JQuery event handler for when we hit reset once we fill in information to reset our password.
 // Process reset data into json file
 $('.reset-form').on('submit', function(){
       url = $(this).attr('action');
       method = $(this).attr('method');
       $('.reset-info').find('.form-control').each(function(index,value){
-        console.log(value);
+        data[$(this).attr('name')] = $(this).val();
       });
 
     return false;
 });
-// Fetch privilage lvl
-function fetchPriv(lvl) {
-  switch (lvl) {
-    case 0:
-      return "";
-      break;
-    case 1:
-      return "gensuptswbd.html";
-      break;
-    case 2:
-      return "supvswbd.html";
-      break;
-    case 3:
-      return "tehpswbd.html";
-      break;
-    default:
-      return null;
-  }
-
-}
 // Process login data
 $('.login-form').on('submit',function(){
-  method = $(this).attr('method');
-  url = $(this).attr('action');
-  // for each '.form-control' inputs (usr, passwd) in login-info, append data into our obj
+
+  // for each div element inside the class 'login-info'
+  // find each div element whose class is 'form control' and append its value
+  // into our obj data
   $('.login-info').find('.form-control').each(function(index,value) {
       data[$(this).attr('name')] = $(this).val();
   });
-  // At this point we have object {usr: "xxx", passwd: "xxxx"} , now we stringify and send client request
-  var clientR = new XMLHttpRequest();
-  data = JSON.stringify(data);
+
+
+  // At this point we have object {usr: "xxx", passwd: "xxxx"} , now we send a request
+  // to the server
+
+  method = $(this).attr('method'); // server needs a method to send its data
+  url = $(this).attr('action'); // it needs a url to insert its request into
+  var clientR = new XMLHttpRequest(); // create request object
+  data = JSON.stringify(data); // cast our obj into string, specifically a JSON file
+  // STEPS
+  //  1.  When we create our request object, we need to call the open method, which
+  //  essentially is the physical communication that the client begins with the server
+  //  .send(params) =(send method , url go to, async)
+  /*
+      async: boolean that indicates whether or not to perform the operation asynchronously. i.e.
+
+      If this value is false, the send() method does not return until a response from the server is received.
+      If true, notification of a completed transaction is provided using event listeners.
+      This must be true if the multipart attribute is true, or an exception will be thrown.
+  */
   clientR.open(method,url, true);
   clientR.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+  // This is our event listener, an object property whose value is an anon. function that checks to
+  // see if our request was send successfully
   clientR.onreadystatechange = function () {
     // if our request tranfer is complete and http status is OK
     if (clientR.readyState == 4 && clientR.status == 200) {
         try {
           // responsedata stores our login user info if user is authenticated
           responsedata = JSON.parse(clientR.responseText);
-          console.log(responsedata);
-          htmlRedirect = fetchPriv(responsedata['PRIVLVL']);
-          if (htmlRedirect=="") {
+          if (responsedata == "") {
               wrongPass();
           }
           else {
+            // Fade out and redirect to new page
             $(document.body).addClass('animated fadeOut');
-              window.location.replace(htmlRedirect);
+            // CODE ...
           }
         } catch (e if e instanceof SyntaxError) {
             wrongPass();
@@ -78,6 +83,9 @@ $('.login-form').on('submit',function(){
   clientR.send(data);
   return false;
 });
+
+// Method that simply clears and animates the password box if wrong password
+// This is js/JQuery/ and animated plugin
 function wrongPass() {
   // input fields incorrect, assuming its password, we clear it
   var aName = "animated shake";
